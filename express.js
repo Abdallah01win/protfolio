41556d2f00b974d3d349eccdb8cb6b4778e3e0fd
Abdallah Bari <abdellah01win@gmail.com>
2022-03-17 23:41:55 +0100

    build home page nav and hero

---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(process.env.PORT || 3003, ()=>{
+    console.log("server up on port 3003")
+})
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
