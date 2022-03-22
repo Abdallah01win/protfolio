5526c673ca3f786a0518af1b2ff1451f1135784a
Abdallah Bari <abdellah01win@gmail.com>
2022-03-22 21:19:53 +0100

    add btn click animation

---
+++ app/index.js
@@ -0,0 +1,31 @@
+const btns = document.getElementsByClassName('btn')
+const btnsMid = document.getElementsByClassName('btn-mid')
+const btnsSml = document.getElementsByClassName('btn-sml')
+
+
+function animateBtn(btn){
+    btn.addEventListener('click', function (e){
+        e.preventDefault;
+        console.log('clicked')
+        let x = e.pageX - e.target.offsetLeft;
+        let y = e.pageY - e.target.offsetTop;
+        let ripples = document.createElement('span');
+        ripples.style.left= x + 'px';
+        ripples.style.top= y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(()=>{
+            ripples.remove()
+        }, 801)
+    })
+}
+
+for (const btn of btns) {
+    animateBtn(btn)
+}
+for (const midBtn of btnsMid) {
+    animateBtn(midBtn)
+}
+for (const smlBtn of btnsSml) {
+    animateBtn(smlBtn)
+}
--- express.js
+++ express.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'app')))
 app.listen(process.env.PORT || 3003, ()=>{
     console.log("server up on port 3003")
 })
