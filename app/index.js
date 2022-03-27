ab33ae2ba45a6ed36d78216e3fb4332da52780a1
Abdallah Bari <abdellah01win@gmail.com>
2022-03-27 22:56:14 +0000

    write project discription & modify animateBtn func

--- app/index.js
+++ app/index.js
@@ -3,28 +3,23 @@ const btnsMid = document.getElementsByClassName('btn-mid')
 const btnsSml = document.getElementsByClassName('btn-sml')
 
 
-function animateBtn(btn){
-    btn.addEventListener('click', function (e){
-        console.log('clicked')
-        let x = e.pageX - e.target.offsetLeft;
-        let y = e.pageY - e.target.offsetTop;
-        let ripples = document.createElement('span');
-        ripples.style.left= x + 'px';
-        ripples.style.top= y + 'px';
-        this.appendChild(ripples);
+function animateBtn(btns) {
+    for (const btn of btns) {
+        btn.addEventListener('click', function (e) {
+            let x = e.pageX - e.target.offsetLeft;
+            let y = e.pageY - e.target.offsetTop;
+            let ripples = document.createElement('span');
+            ripples.style.left = x + 'px';
+            ripples.style.top = y + 'px';
+            this.appendChild(ripples);
 
-        setTimeout(()=>{
-            ripples.remove()
-        }, 801)
-    })
+            setTimeout(() => {
+                ripples.remove()
+            }, 801)
+        })
+    }
 }
 
-for (const btn of btns) {
-    animateBtn(btn)
-}
-for (const midBtn of btnsMid) {
-    animateBtn(midBtn)
-}
-for (const smlBtn of btnsSml) {
-    animateBtn(smlBtn)
-}
+animateBtn(btns);
+animateBtn(btnsMid);
+animateBtn(btnsSml);
